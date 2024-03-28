7aa994e5f9eed4c4174a60597d91600cc7519706
schmidtkesandro <sandroluis@terra.com.br>
2024-03-28 19:08:15 -0300

    otimização do processo

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,7 +18,7 @@ func CEP1() {
 	defer resp.Body.Close()
 
 	// Leia o corpo da resposta
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(io.Reader(resp.Body))
 	if err != nil {
 		fmt.Println("Erro ao ler o corpo da resposta:", err)
 		return
@@ -37,7 +38,7 @@ func CEP2() {
 	defer resp.Body.Close()
 
 	// Leia o corpo da resposta
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(io.Reader(resp.Body))
 	if err != nil {
 		fmt.Println("Erro ao ler o corpo da resposta:", err)
 		return
@@ -47,23 +48,68 @@ func CEP2() {
 	fmt.Println(" ")
 	fmt.Println(string(body))
 	fmt.Println(" ")
+	return
 
+}
+
+// Função que faz a solicitação HTTP e retorna a resposta
+func getResponseBody(url string) ([]byte, error) {
+	// Faz a solicitação GET à URL fornecida
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Lê o corpo da resposta
+	body, err := io.ReadAll(io.Reader(resp.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
 func main() {
 	c1 := make(chan string)
 	c2 := make(chan string)
+	// Recupera os argumentos da linha de comando
+	args := os.Args
+
+	// Verifica se há argumentos suficientes
+	if len(args) < 2 {
+		fmt.Println("Passe o CEP no formato 99999999 como argumento na chamada. Exemplo: go run main.go 01153000 ")
+		return
+	}
+
+	// Recupera o primeiro argumento (o segundo item em args, porque o primeiro é o nome do programa)
+	cep := args[1]
 
 	go func() {
-		//time.Sleep(time.Second)
+		time.Sleep(time.Second)
 
-		CEP1()
+		// Chama a função getResponseBody e obtém a resposta
+		body, err := getResponseBody("http://viacep.com.br/ws/" + cep + "/json/")
+		if err != nil {
+			fmt.Println("Erro ao obter o corpo da resposta:", err)
+			return
+		}
+
+		// Imprime o corpo da resposta
+		fmt.Println(string(body))
 		c1 <- "Via CEP"
-
 	}()
 
 	go func() {
+		// Chama a função getResponseBody e obtém a resposta
+		body, err := getResponseBody("https://brasilapi.com.br/api/cep/v1/" + cep + " + cep/")
+		if err != nil {
+			fmt.Println("Erro ao obter o corpo da resposta:", err)
+			return
+		}
 
-		CEP2()
+		// Imprime o corpo da resposta
+		fmt.Println(string(body))
+
 		c2 <- "Brasil API"
 	}()
 
